022c0a005a682b3b9980f98b05fa7876e6af29b4
JingjieGao <jgao2@madisoncollege.edu>
2025-02-26 22:44:42 -0600

    Add the cleandb.sql to clean up or reset a database

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: recipe_store
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `recipes`
+--
+
+DROP TABLE IF EXISTS `recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recipes` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `user_id` int DEFAULT NULL,
+                           `name` varchar(100) DEFAULT NULL,
+                           `category` varchar(100) DEFAULT NULL,
+                           `ingredients` text,
+                           `instructions` text,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `recipes_name_uindex` (`name`),
+                           KEY `recipes_user_id_fk` (`user_id`),
+                           CONSTRAINT `recipes_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipes`
+--
+
+LOCK TABLES `recipes` WRITE;
+/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
+INSERT INTO `recipes` VALUES (1,2,'Chicken Curry','Curry','Chicken, Curry Powder, Coconut Milk, Onion, Garlic, Ginger, Salt, Pepper','1. Fry the chicken with onions and garlic. 2. Add curry powder and coconut milk. 3. Simmer and serve with rice.'),(2,1,'Vegetable Stir Fry','Vegetarian','Carrot, Bell Pepper, Broccoli, Soy Sauce, Garlic, Olive Oil','1. Cut the vegetables. 2. Stir fry with garlic and soy sauce. 3. Serve with rice.'),(3,3,'Beef Tacos','Mexican','Ground Beef, Taco Shells, Lettuce, Tomato, Cheese, Sour Cream, Salsa','1. Cook the beef. 2. Fill the taco shells with beef and toppings. 3. Serve with salsa and sour cream.');
+/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `first_name` varchar(50) NOT NULL,
+                        `last_name` varchar(50) NOT NULL,
+                        `email` varchar(100) NOT NULL,
+                        `password` varchar(100) NOT NULL,
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `user_email_uindex` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Jane','Gao','jane@example.com','password123'),(2,'Sam','Smith','sam@example.com','password456'),(3,'Emma','Davis','emma@example.com','password789');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-26 17:10:53
