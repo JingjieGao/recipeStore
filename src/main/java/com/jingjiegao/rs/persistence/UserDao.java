9dabe5a8b39272870817c856caf4e10a2350dd2e
JingjieGao <jgao2@madisoncollege.edu>
2025-02-26 22:33:55 -0600

    Create the UserDao and implement CRUD methods in it

--- src/main/java/com/jingjiegao/rs/persistence/UserDao.java
+++ src/main/java/com/jingjiegao/rs/persistence/UserDao.java
@@ -1,4 +1,141 @@
 package com.jingjiegao.rs.persistence;
 
+import com.jingjiegao.rs.entity.User;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Root;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import java.util.List;
+
+
+/**
+ * The type User dao.
+ */
 public class UserDao {
+    private static final Logger logger = LogManager.getLogger(UserDao.class);
+    /**
+     * The Session factory.
+     */
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Gets by id.
+     *
+     * @param id the id
+     * @return the by id
+     */
+    public User getById(int id) {
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    /**
+     * Update.
+     *
+     * @param user the user
+     */
+    public void update(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(user);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Insert int.
+     *
+     * @param user the user
+     * @return the int
+     */
+    public int insert(User user) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(user);
+        transaction.commit();
+        id = user.getId();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete.
+     *
+     * @param user the user
+     */
+    public void delete(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Gets all.
+     *
+     * @return the all
+     */
+    public List<User> getAll() {
+        Session session = sessionFactory.openSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createSelectionQuery( query ).getResultList();
+
+        logger.debug("The list of authors " + users);
+        session.close();
+        return users;
+    }
+
+    /**
+     * Gets by property equal.
+     *
+     * @param propertyName the property name
+     * @param value        the value
+     * @return the by property equal
+     */
+    public List<User> getByPropertyEqual(String propertyName, String value) {
+        Session session = sessionFactory.openSession();
+        logger.debug("Searching for user with " + propertyName + " = " + value);
+
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root).where(builder.equal(root.get(propertyName), value));
+        List<User> users = session.createSelectionQuery(query).getResultList();
+
+        session.close();
+        return users;
+    }
+
+    /**
+     * Gets by property like.
+     *
+     * @param propertyName the property name
+     * @param value        the value
+     * @return the by property like
+     */
+    public List<User> getByPropertyLike(String propertyName, String value) {
+        Session session = sessionFactory.openSession();
+        logger.debug("Searching for user with {} = {}",  propertyName, value);
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        Expression<String> propertyPath = root.get(propertyName);
+        query.where(builder.like(propertyPath, "%" + value + "%"));
+        List<User> users = session.createSelectionQuery(query).getResultList();
+        session.close();
+        return users;
+    }
+
+
 }
