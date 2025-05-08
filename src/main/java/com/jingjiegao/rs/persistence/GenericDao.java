d6dd1a3a3dde9fc0897e63426ab6e962e6436e19
Jingjie Gao <jgao2@madisoncollege.edu>
2025-05-07 19:44:37 -0500

    Created the GenericDao for basic CRUD functions

--- src/main/java/com/jingjiegao/rs/persistence/GenericDao.java
+++ src/main/java/com/jingjiegao/rs/persistence/GenericDao.java
@@ -1,4 +1,171 @@
 package com.jingjiegao.rs.persistence;
 
-public class GenericDao {
+import jakarta.persistence.criteria.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The type Generic dao.
+ *
+ * @param <T> the type parameter
+ */
+public class GenericDao<T> {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private final Class<T> type;
+
+    /**
+     * Instantiates a new Generic dao.
+     *
+     * @param type the type
+     */
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets by id.
+     *
+     * @param id the id
+     * @return the by id
+     */
+    public T getById(int id) {
+        Session session = getSession();
+        T entity = session.get(type, id);
+        session.close();
+        return entity;
+    }
+
+    /**
+     * Save or update.
+     *
+     * @param entity the entity
+     */
+    public void Update(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Insert int.
+     *
+     * @param entity the entity
+     * @return the int
+     */
+    public int insert(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(entity);
+        transaction.commit();
+        int id = (Integer) session.getIdentifier(entity);
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete.
+     *
+     * @param entity the entity
+     */
+    public void delete(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Gets all.
+     *
+     * @return the all
+     */
+    public List<T> getAll() {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        query.select(root);
+        List<T> entities = session.createSelectionQuery(query).getResultList();
+        session.close();
+        return entities;
+    }
+
+    /**
+     * Gets by property equal.
+     *
+     * @param propertyName the property name
+     * @param value        the value
+     * @return the by property equal
+     */
+    public List<T> getByPropertyEqual(String propertyName, Object value) {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        query.select(root).where(builder.equal(root.get(propertyName), value));
+        List<T> entities = session.createSelectionQuery(query).getResultList();
+        session.close();
+        return entities;
+    }
+
+    /**
+     * Find by property equal list.
+     *
+     * @param propertyMap the property map
+     * @return the list
+     */
+    public List<T> findByPropertyEqual(Map<String, Object> propertyMap) {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<Predicate> predicates = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : propertyMap.entrySet()) {
+            predicates.add(builder.equal(root.get(entry.getKey()), entry.getValue()));
+        }
+        query.select(root).where(builder.and(predicates.toArray(new Predicate[0])));
+        List<T> items = session.createSelectionQuery(query).getResultList();
+        session.close();
+        return items;
+    }
+
+    /**
+     * Gets by property like.
+     *
+     * @param propertyName the property name
+     * @param value        the value
+     * @return the by property like
+     */
+    public List<T> getByPropertyLike(String propertyName, String value) {
+        Session session = getSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        Expression<String> propertyPath = root.get(propertyName);
+        query.where(builder.like(propertyPath, "%" + value + "%"));
+        List<T> entities = session.createSelectionQuery(query).getResultList();
+        session.close();
+        return entities;
+    }
+
+    /**
+     * Gets session.
+     *
+     * @return the session
+     */
+    public Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
 }
